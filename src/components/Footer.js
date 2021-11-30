d1fb1dc8df7c34bd85dd762d50360615dd89f8da
Christopher Harden <Harden.Christopher00@gmail.com>
2021-11-30 09:49:44 -0600

    changed root files and finished day one work

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+               Author: Chris Harden 
+            </div>
+        );
+    }
+}
+
+export default Footer;
