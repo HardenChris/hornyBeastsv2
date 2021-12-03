e665b65d465356d3ba7d08004c2652bd0792c167
Christopher Harden <Harden.Christopher00@gmail.com>
2021-12-03 08:59:00 -0600

    form is finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,11 @@ import { Container } from 'react-bootstrap';
 class Main extends Component {
     
     render() {
-        let beastBoy = this.props.beastAll.map(beast => <HornedBeasts newModalBeast={this.props.newModalBeast} beast={beast}/>)
+        let beastyBoys = this.props.beastAll.map(beast => <HornedBeasts newModalBeast={this.props.newModalBeast} beast={beast}/>)
         return (
             <Container>
                  <Row xs={1} sm={2} md={3} lg={4} xl={7}>
-                    {beastBoy}
+                    {beastyBoys}
                 </Row>
             </Container>
         );
